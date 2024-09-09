b4b0bdd5bec76c2688cf35ba02a074f427c7d319
geoffreygarrett <g.h.garrett13@gmail.com>
2024-09-09 21:37:20 +0200

    fix: droid

--- nix/home/modules/android/default.nix
+++ nix/home/modules/android/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, services, ... }:
 let
   #  # Load the cachix configuration but do not immediately incorporate it into the system configuration
   #  cachixConfig = import ../shared/cachix { inherit pkgs lib; };
