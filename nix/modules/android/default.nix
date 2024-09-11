5b86568afc9171a2bf5d72e6853dec39ef7cb8c6
geoffreygarrett <g.h.garrett13@gmail.com>
2024-09-11 02:13:18 +0200

    feat: sops key sync

--- nix/modules/android/default.nix
+++ nix/modules/android/default.nix
@@ -95,17 +95,17 @@
   # };
 
   # Android Integration
-  # android-integration = {
-  #   am.enable = false;
-  #   termux-open.enable = false;
-  #   termux-open-url.enable = false;
-  #   termux-reload-settings.enable = false;
-  #   termux-setup-storage.enable = false;
-  #   termux-wake-lock.enable = false;
-  #   termux-wake-unlock.enable = false;
-  #   unsupported.enable = false;
-  #   xdg-open.enable = false;
-  # };
+  android-integration = {
+    am.enable = true;
+    termux-open.enable = true;
+    termux-open-url.enable = true;
+    termux-reload-settings.enable = true;
+    termux-setup-storage.enable = true;
+    termux-wake-lock.enable = true;
+    termux-wake-unlock.enable = true;
+    unsupported.enable = false;
+    xdg-open.enable = true;
+  };
 
   # Home Manager Configuration
   home-manager = {
