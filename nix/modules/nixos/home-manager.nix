b890fa9462bc800c069451533c50661036751f26
geoffreygarrett <geoffrey@apollo.xyz>
2024-10-02 11:08:35 +0200

    fix: key-repeat

--- nix/modules/nixos/home-manager.nix
+++ nix/modules/nixos/home-manager.nix
@@ -29,37 +29,39 @@ in
       "application/x-shellscript" = [ "alacritty-neovim.desktop" ];
     };
   };
-
+  xsession.initExtra = ''
+    ${pkgs.xorg.xset}/bin/xset r rate 200 45
+  '';
   home = {
     enableNixpkgsReleaseCheck = false;
     packages = pkgs.callPackage ./packages.nix { };
 
-    file = lib.mkMerge [
-      linux-desktop-files
-      {
-        ".Xresources".text = ''
-          Xft.dpi: 96
-          Xft.antialias: true
-          Xft.hinting: true
-          Xft.rgba: rgb
-          Xft.autohint: false
-          Xft.hintstyle: hintslight
-          Xft.lcdfilter: lcddefault
-
-          ! Set key repeat rate
-          Xkb.repeatDelay: 225
-          Xkb.repeatInterval: 25
-        '';
-
-        ".xprofile".text = ''
-          # Set key repeat rate
-          xset r rate 225 25
-        '';
-      }
-      # Uncomment the following line if you have additional files to import
-      # (import ./files.nix { inherit user pkgs; })
-    ];
-
+    # file = lib.mkMerge [
+    #   linux-desktop-files
+    #   {
+    #     ".Xresources".text = ''
+    #       Xft.dpi: 96
+    #       Xft.antialias: true
+    #       Xft.hinting: true
+    #       Xft.rgba: rgb
+    #       Xft.autohint: false
+    #       Xft.hintstyle: hintslight
+    #       Xft.lcdfilter: lcddefault
+    #
+    #       ! Set key repeat rate
+    #       Xkb.repeatDelay: 225
+    #       Xkb.repeatInterval: 25
+    #     '';
+    #
+    #     ".xprofile".text = ''
+    #       # Set key repeat rate
+    #       xset r rate 225 25
+    #     '';
+    #   }
+    #   # Uncomment the following line if you have additional files to import
+    #   # (import ./files.nix { inherit user pkgs; })
+    # ];
+    #
     stateVersion = "24.05";
   };
 
